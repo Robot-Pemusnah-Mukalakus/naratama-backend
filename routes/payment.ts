0cd8412137fd7120e3df8e39e00ee5ce9a827e28
SentientCorn <radityanarotama0@gmail.com>
2025-11-18 02:01:10 +0700

    fix: add endpoint for finishing membership transactions

--- routes/payment.ts
+++ routes/payment.ts
@@ -2,12 +2,13 @@ import { Prisma } from "@prisma/client";
 import express from "express";
 
 import prisma from "../lib/prisma.js";
-import { createTransactionMembership, createTransactionRoomBooking } from "#config/midtrans.js";
+import { createTransactionMembership, createTransactionRoomBooking, finishTransactionMembership } from "#config/midtrans.js";
 
 const router = express.Router();
 
 // POST /api/payment/membership
 router.post("/membership", createTransactionMembership);
+router.post("/membership/finish", finishTransactionMembership);
 router.post("/room-booking", createTransactionRoomBooking);
 
 export default router;
